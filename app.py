54609567d1ef3db3b4b30dd91df9491aeb6b2cd1
stefania <stefaniacobalondon@gmail.com>
2026-03-27 16:27:13 -0500

    first commit

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from app.routes.series_routes import series_bp
+from app import create_app 
+
+app = create_app() 
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(series_bp)
     return app
 
-# SACA ESTO DEL IF
 app = create_app()
 
 if __name__ == "__main__":
